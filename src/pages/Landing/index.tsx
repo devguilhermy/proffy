4439e7d7dd51ccfca84f7498a241d81cf66a39d6
Guilhermy <guilhermycam@gmail.com>
2020-08-03 21:44:45 -0300

    Mobile website done

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./styles.css";
+
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import teachIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+function Landing() {
+    return (
+        <div id="landing-page">
+            <div id="landing-page-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy Logo" />
+                    <h2>Sua plataforma de estudos online</h2>
+                </div>
+                <img
+                    src={landingImg}
+                    alt=""
+                    className="hero-image"
+                />
+
+                <div className="buttons-container">
+                    <a href="#" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </a>
+                    <a href="#" className="teach">
+                        <img src={teachIcon} alt="Ensinar"/>
+                        Ensinar
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Já foram realizadas 3500 conexões pela plataforma
+                    <img src={purpleHeartIcon} alt="Coração roxo"/>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
